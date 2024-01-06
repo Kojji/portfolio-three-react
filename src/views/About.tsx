8d0fb7e179417d7dec19fc2b938ca76260f7bfb7
Kojji <fernandokojiyama@gmail.com>
2024-01-06 20:23:07 +1000

    add: tab info and about section update

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -2,11 +2,17 @@ import { useTranslation } from "react-i18next";
 
 function About() {
   const { t } = useTranslation();
+
+  const sectionItemsInArray: number = Number(
+    t(`InfoCards.Personal.Paragraphs.length`)
+  );
+
   function getParagraphs() {
     let content = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < sectionItemsInArray; i++) {
       content.push(
         <div
+          className="pb-3"
           key={i}
           dangerouslySetInnerHTML={{
             __html: t(`InfoCards.Personal.Paragraphs.${i}`),
@@ -20,7 +26,7 @@ function About() {
   return (
     <div
       id="about-section"
-      className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10"
+      className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 max-w-screen-md"
     >
       <p className="text-3xl text-gray-700 font-bold mb-5">
         {t(`InfoCards.Personal.PageTitle`)}
